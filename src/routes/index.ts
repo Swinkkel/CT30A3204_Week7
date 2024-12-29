b2125fe62f8da6b9ba94918ef9ebfe4916cad3af
Tomi Helin <tomi.helin@student.lut.fi>
2024-12-29 11:50:22 +0200

    Task 2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import {Request, Response, Router} from "express"
 import bcrypt from "bcrypt"
+import jwt, {JwtPayload} from "jsonwebtoken"
 import { compile } from "morgan"
 
 const router: Router = Router()
@@ -28,6 +29,29 @@ router.post("/api/user/register", async (req: Request, res: Response) => {
 
 router.get('/api/user/list', (req: Request, res: Response) => {
     res.status(200).json(users);
+})
+
+router.post("/api/user/login", async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+
+    const loginUser = users.find((user) => user.email === email);
+    if (!loginUser) {
+        res.status(401).json({ message: 'Login failed.' });
+        return 
+    }
+
+    if (!bcrypt.compareSync(password, loginUser.password)) {
+        res.status(401).json({ message: 'Login failed.' });
+        return
+    }
+
+    const jwtPayload: JwtPayload = {
+        email: email
+    }
+    const token: string = jwt.sign(jwtPayload, process.env.SECRET as string, { expiresIn: "2m"})
+
+    res.status(200).json({success: true, token})  
+    return
 })
 
 export default router
